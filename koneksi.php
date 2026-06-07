82ba19c7261ab358fca442f6b4e6bd166a0087ea
gideonsiahaan <gideonsiahaan08@gmail.com>
2026-06-08 00:41:23 +0700

    Refactor database connection code with error display

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,16 @@
 <?php
+// Tiga baris ini untuk memaksa PHP menampilkan error di layar
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$MYSQLHOST = "mysql.railway.internal"; 
-$MYSQLUSER = "root"; 
-$MYSQLPASSWORD = "AoCuTLCfUPvvpitvGkxHZqRuEVVHWLqE"; 
-$MYSQLDATABASE= "railway"; 
-$MYSQLPORT = "3306"; 
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "AoCuTLCfUPvvpitvGkxHZqRuEVVHWLqE";
+$db = "railway";
+$port = "3306";
 
-$conn = mysqli_connect($MYSQLHOST, $MYSQLUSER, $MYSQLPASSWORD, $MYSQLDATABASE, $MYSQLPORT);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi ke database gagal: " . mysqli_connect_error());
